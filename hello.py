6c943f5818eff28ffb818762f27a87845869065f
Ankit7839 <36545434+Ankit7839@users.noreply.github.com>
2018-08-05 01:18:06 +0530

    print new update

--- hello.py
+++ hello.py
@@ -1,12 +1,13 @@
 print("Hello World")
 print("hello world")
-#or you also use this way
+or you also use this way
 print('hello "I am ankit" world')
-#or this way 
+or this way 
 print("hello 'ANKIT' world")
-#or
+or
 print("hello \"world\" world")
-#so you can use ""  or '' many type..
+so you can use ""  or '' many type..
 print('I\'m Ankit')
 print("Ankit\nAsthana")
 print("NAME\tAnkit")
+
